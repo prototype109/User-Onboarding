386d789ef6e53c6e7191170a52f30564e0522d93
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-15 20:14:00 -0500

    Added validationSchema to form

--- advanced-form-management/src/components/Form.js
+++ advanced-form-management/src/components/Form.js
@@ -3,30 +3,34 @@ import { withFormik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function NewUser(){
+function NewUser({ errors, values, touched }){
     return(
         <Form>
+            {touched.name && errors.name && <p>{errors.name}</p>}
             <Field component='input' 
                     type='text' 
                     name='name' 
                     placeholder='Name' 
                     />
+            {touched.email && errors.email && <p>{errors.email}</p>}
             <Field component='input' 
                     type='email' 
                     name='email' 
                     placeholder='Email' 
                     />
+            {touched.password && errors.password && <p>{errors.password}</p>}
             <Field component='input' 
                     type='password' 
                     name='password' 
                     placeholder='Password' 
                     />
+            {errors.tos && <p>{errors.tos}</p>}
             <label>
                 Accept Terms of Service:
             <Field type='checkbox' 
                     name='tos' 
                     placeholder='Terms Of Service'
-                    checked={false}
+                    checked={values.tos}
                     />
             </label>
             <button type='submit'>Submit</button>
@@ -42,7 +46,15 @@ const NewUserFormik = withFormik({
             password: password || '',
             tos: tos || ''
         };
-    }
+    },
+    validationSchema: Yup.object().shape(
+        {
+            name: Yup.string().required('Name field not filled!'),
+            email: Yup.string().required('Email field not filled!'),
+            password: Yup.string().required().min(8, 'password must be 8 characters long'),
+            tos: Yup.bool().required('Must Accept Terms Of Service to proceed')
+        }
+    )
 })(NewUser);
 
 export default NewUserFormik;
